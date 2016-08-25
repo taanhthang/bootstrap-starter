39a1cc4bbe5e6550ad2f70e45dba47555f68340c
taanhthang <taanhthang@gmail.com>
2016-08-25 15:21:18 +0200

    click me hide

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,7 @@ DB.connect(app);
 DB.ready(function() {
     $('#hello').html('Connected to Baqend app <strong>' + app + "</strong>");
 });
+DB.ready(function() {
+    $('#yes').click(function(){
+    $(this).hide(1000);})
+});
