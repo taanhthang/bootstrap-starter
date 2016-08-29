01b5e88e6865b8ac6a31a689266b451a354e51fa
Friedrich <fbraun2403@yahoo.de>
2016-08-29 15:49:13 +0200

    ProduktAnsicht Beispiel Seite

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ var app = 'thabefrijo';
 DB.connect(app);
 
 DB.ready(function() {
-    $('#hello').html('Connected to Baqend app <strong>' + app + "</strong>");
+    $('#hello').html('dit con me <strong>' + app + "</strong>");
 });
 DB.ready(function() {
     $('#yes').click(function(){
