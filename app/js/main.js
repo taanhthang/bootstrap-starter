564363b3a0517470600c1d5eca72f7112a5e4c3f
HaosImperia <b.cordt@hotmail.de>
2016-09-01 12:54:58 +0200

    Merge branch 'master' of C:\Users\Imperia\WebstormProjects\bootstrap-starter with conflicts.

--- app/js/main.js
+++ app/js/main.js
@@ -51,3 +51,66 @@ DB.connect(app).then()
  $(this).hide(1000);})
  });
  */
+
+var divMoreInfo = null;
+
+Init();
+
+function Init()
+{
+    divMoreInfo = document.getElementById("divMoreInfo");
+}
+
+function func1(obj)
+{
+    obj.style.width = "450px";
+    obj.style.height = "660px";
+
+    divMoreInfo.style.left = (obj.offsetLeft + 280) + "px";
+    divMoreInfo.style.top = obj.offsetTop + "px";
+
+    divMoreInfo.style.display = "";
+    divMoreInfo.style.visibility = "visible";
+}
+
+function func2(obj)
+{
+    obj.style.width = "256px";
+    obj.style.height = "356px";
+
+    divMoreInfo.style.display = "none";
+    divMoreInfo.style.visibility = "hidden";
+}
+
+/* Webshop*/
+
+function sortdiv() {
+    var container = document.getElementById("list");
+    var elements = container.childNodes;
+    var sortMe = [];
+    for (var i=0; i<elements.length; i++) {
+        if (!elements[i].id) {
+            continue;
+        }
+        var sortPart = elements[i].id.split("-");
+        if (sortPart.length > 1) {
+            sortMe.push([ 1 * sortPart[1] , elements[i] ]);
+        }
+    }
+    sortMe.sort(function(x, y) {
+        return x[0] - y[0];
+    });
+    for (var i=0; i<sortMe.length; i++) {
+        container.appendChild(sortMe[i][1]);
+    }
+
+
+    /*Alternative Sort-function
+    var main = document.getElementById( 'list' );
+
+    [].map.call( main.children, Object ).sort( function ( a, b ) {
+        return +a.id.match( /\d+/ ) - +b.id.match( /\d+/ );
+    }).forEach( function ( elem ) {
+        main.appendChild( elem );
+    });*/
+}
