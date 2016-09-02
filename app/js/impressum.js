86313755a494afd9307006548cd1466a3476fe1a
jovanaBunjevac <jovana.bunjevac@gmail.com>
2016-09-02 15:21:20 +0200

    schwarz-weiß logo

--- app/js/impressum.js
+++ app/js/impressum.js
@@ -1,3 +1,29 @@
 /**
  * Created by jovanabunjevac on 02.09.16.
  */
+
+function validateForm() {
+    var x = document.forms["contactForm"]["usr"].value;
+    if (x == null || x == "") {
+        alert("Name field must be filled out");
+        return false;
+    }
+
+    var x = document.forms["contactForm"]["email"].value;
+    if (x == null || x == "") {
+        alert("E-mail field must be filled out");
+        return false;
+    }
+
+    var x = document.forms["contactForm"]["message"].value;
+    if (x == null || x == "") {
+        alert("Message field must be filled out");
+        return false;
+    }
+
+    var x = document.forms["contactForm"]["question"].value;
+    if (x == null || x == "") {
+        alert("Answer field must be filled out");
+        return false;
+    }
+}
