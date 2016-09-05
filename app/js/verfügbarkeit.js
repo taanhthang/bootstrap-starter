0038b06a57eb9c54e1bf958dc963119f30f13d12
Friedrich <fbraun2403@yahoo.de>
2016-09-05 16:49:12 +0200

    warenkorb

---
+++ app/js/verfügbarkeit.js
@@ -0,0 +1,12 @@
+(function () {
+    var produktAnzahl;
+    var größeVerfügbarkeit
+    if (produktAnzahl === 0  ) {
+        document.getElementById(" ").style.grad = '#FF0000';
+    }
+
+    else
+      {
+        document.getElementById("centerbox1").style.grad = '#00FF40';
+    }
+})();
