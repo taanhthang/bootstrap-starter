f1bf2154f59e6b4c16936d8af81da540c36c4e4e
HaosImperia <b.cordt@hotmail.de>
2016-09-07 12:31:37 +0200

    webshop  ausführliche Kommentare

--- app/js/webShop.js
+++ app/js/webShop.js
@@ -2,6 +2,16 @@
  * Created by Imperia on 01.09.2016.
  */
 /* Webshop*/
+
+
+/*!!!!!!!!!Achtung es funktioniert , ist aber nicht die beste , aber eine funktionierende Variante!!!!!!!!!!!*/
+
+
+/*Alle Funktionen werden erst bei drücken der list-group elemente auf der Shoppage ausgeführt,also erst
+durch einwirken des Users ausgeführt
+ */
+
+
 /* SortTops id"-1", show id am ende */
 function sortdivTops() {
 
@@ -25,12 +35,11 @@ function sortdivTops() {
     for (var i = 0; i < sortMe.length; i++) {
         container.appendChild(sortMe[i][1]);
     }
-    for (var j = 1; j < 4; j++) {
-        hide(document.getElementById('/db/produkt/' + [j] + '-2'));
-        hide(document.getElementById('/db/produkt/' + [j] + '-3'));
-        hide(document.getElementById('/db/produkt/' + [j] + '-4'));
-        show(document.getElementById('/db/produkt/' + [j] + '-1'));
-    }
+    /*Großes Read me unten lesen  */
+    hideallJacken();
+    hideallShirt();
+    hideallPullover();
+    showallTops();
 }
 
 
@@ -57,12 +66,11 @@ function sortdivShirt() {
         container.appendChild(sortMe[i][1]);
 
     }
-    for (var j = 1; j < 4; j++) {
-        hide(document.getElementById('/db/produkt/' + [j] + '-1'));
-        hide(document.getElementById('/db/produkt/' + [j] + '-3'));
-        hide(document.getElementById('/db/produkt/' + [j] + '-4'));
-        show(document.getElementById('/db/produkt/' + [j] + '-2'));
-    }
+    /*Großes Read me unten lesen  */
+    hideallTops();
+    hideallJacken();
+    hideallPullover();
+    showallShirt();
 }
 
 
@@ -89,12 +97,11 @@ function sortdivPullover() {
         container.appendChild(sortMe[i][1]);
 
     }
-    for (var j = 1; j < 4; j++) {
-        hide(document.getElementById('/db/produkt/' + [j] + '-1'));
-        hide(document.getElementById('/db/produkt/' + [j] + '-2'));
-        hide(document.getElementById('/db/produkt/' + [j] + '-4'));
-        show(document.getElementById('/db/produkt/' + [j] + '-3'));
-    }
+    /*Großes Read me unten lesen  */
+    hideallTops();
+    hideallShirt();
+    hideallJacken();
+    showllPullover();
 
 }
 
@@ -121,13 +128,12 @@ function sortdivJacken() {
         container.appendChild(sortMe[i][1]);
 
     }
+    /*Großes Read me unten lesen  */
 
-    for (var j = 1; j < 4; j++) {
-        hide(document.getElementById('/db/produkt/' + [j] + '-1'));
-        hide(document.getElementById('/db/produkt/' + [j] + '-2'));
-        hide(document.getElementById('/db/produkt/' + [j] + '-3'));
-        show(document.getElementById('/db/produkt/' + [j] + '-4'));
-    }
+    hideallTops();
+    hideallShirt();
+    hideallPullover();
+    showallJacken();
 
 }
 
@@ -154,5 +160,82 @@ function show(which) {
     }
     else {
         return;
+    }
+}
+
+
+/*READ ME PLEASE:Diese Funktion sind unschön funktionieren aber , es geht bestimmt schöner über J-querry
+ ich habe es damit aber nicht hinbekommen(Felix Tip konnte ich so einfach auch nicht umsetzen, da zu geringe
+ Programmiererfahrung und anderer wichtigere Aufgaben):
+
+ Die Funktion FUnktioniert mit Jj<4 da die Anzahl der Kategorieprodukte zur Zeit immer 3 ist ,
+ Die Funktion hat auch einbandfrei das gemacht was es soll wenn j<100 ist gab nur den blöden Sicherheitsfehler
+ TypeError aus wenn es zu Ids kamm die nicht vergeben waren:Es hat aber Trotzdem das dargestellt was es soll.
+
+ Deshalb habe ich zur leichten Anpassung es jetz inm die repetativenFunktionen da unten geschrieben, die ich weiß nicht
+ schön sind aber funktionieren.
+ */
+
+
+/*Für Tops zur Zeit 3 in DB */
+
+function hideallTops(){
+    for (var j = 1; j < 4; j++) {
+        hide(document.getElementById('/db/produkt/' + j + '-1'));
+    }
+}
+function showallTops(){
+    for (var j = 1; j < 4; j++) {
+        show(document.getElementById('/db/produkt/' + j + '-1'));
+    }
+}
+
+/*Für Shirt zur Zeit 3 in DB */
+
+
+
+
+function hideallShirt(){
+    for (var j = 1; j < 4; j++) {
+        hide(document.getElementById('/db/produkt/' + j + '-2'));
+    }
+}
+
+function showallShirt(){
+    for (var j = 1; j < 4; j++) {
+        show(document.getElementById('/db/produkt/' + j + '-2'));
+    }
+}
+
+
+/*Für Pullover zur Zeit 3 in DB */
+
+
+
+function hideallPullover(){
+    for (var j = 1; j < 4; j++) {
+        hide(document.getElementById('/db/produkt/' + j + '-3'));
+    }
+}
+
+function showllPullover(){
+    for (var j = 1; j < 4; j++) {
+        show(document.getElementById('/db/produkt/' + j + '-3'));
+    }
+}
+
+/*Für Jacken zur Zeit 3 in DB */
+
+
+
+function hideallJacken(){
+    for (var j = 1; j < 4; j++) {
+        hide(document.getElementById('/db/produkt/' + j + '-4'));
+    }
+}
+
+function showallJacken(){
+    for (var j = 1; j < 4; j++) {
+        show(document.getElementById('/db/produkt/' + j + '-4'));
     }
 }
